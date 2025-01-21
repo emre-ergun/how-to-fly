8aac8e6d40fd260c8c6d865e9fd78590bf30cb85
Emre Ergun <emreergun@engramsoft.com>
2025-01-21 16:14:17 +0300

    triangles drawn for representing the animals(birds) at the frontend

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 use nalgebra as na;
 use rand::{Rng, RngCore};
 
---
+++ www/src/main.js
@@ -0,0 +1,35 @@
+import * as sim from 'lib-simulation-wasm';
+
+CanvasRenderingContext2D.prototype.drawTriangle = function (x, y, size) {
+  this.beginPath();
+  this.moveTo(x, y);
+  this.lineTo(x + size, y + size);
+  this.lineTo(x - size, y + size);
+  this.lineTo(x, y);
+
+  this.fillStyle = 'rgb(0, 0, 0)';
+  this.fill();
+};
+
+const simulation = new sim.Simulation();
+const viewport = document.getElementById('viewport');
+
+const viewportWidth = viewport.width;
+const viewportHeight = viewport.height;
+const viewportScale = window.devicePixelRatio || 1;
+
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
+const ctxt = viewport.getContext('2d');
+ctxt.scale(viewportScale, viewportScale);
+
+for (const animal of simulation.world().animals) {
+  ctxt.drawTriangle(
+    animal.x * viewportWidth,
+    animal.y * viewportHeight,
+    0.01 * viewportWidth
+  );
+}
