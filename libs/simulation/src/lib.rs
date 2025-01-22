896c37fa65840d3d4a036f0cb5114217f41e59a7
Emre Ergun <emreergun@engramsoft.com>
2025-01-22 17:41:42 +0300

    brain and eye implemented to the animal and simulation updated to follow foods

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -6,8 +6,15 @@ mod food;
 mod world;
 
 pub use self::{animal::*, eye::*, food::*, world::*};
+use lib_neural_network as nn;
 use nalgebra as na;
 use rand::{Rng, RngCore};
+use std::f32::consts::FRAC_PI_2;
+
+const SPEED_MIN: f32 = 0.001;
+const SPEED_MAX: f32 = 0.005;
+const SPEED_ACCEL: f32 = 0.2;
+const ROTATION_ACCEL: f32 = FRAC_PI_2;
 
 pub struct Simulation {
     world: World,
@@ -26,6 +33,7 @@ impl Simulation {
 
     pub fn step(&mut self, rng: &mut dyn RngCore) {
         self.process_collisions(rng);
+        self.process_brains();
         self.process_movements();
     }
 
@@ -35,6 +43,22 @@ impl Simulation {
 
             animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
             animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
+        }
+    }
+
+    fn process_brains(&mut self) {
+        for animal in &mut self.world.animals {
+            let vision =
+                animal
+                    .eye
+                    .process_vision(animal.position, animal.rotation, &self.world.foods);
+            let response = animal.brain.propagate(vision);
+
+            let speed = response[0].clamp(-SPEED_ACCEL, SPEED_ACCEL);
+            let rotation = response[1].clamp(-ROTATION_ACCEL, ROTATION_ACCEL);
+
+            animal.speed = (animal.speed + speed).clamp(SPEED_MIN, SPEED_MAX);
+            animal.rotation = na::Rotation2::new(animal.rotation.angle() + rotation);
         }
     }
 
