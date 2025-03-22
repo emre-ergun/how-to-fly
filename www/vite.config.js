6fb7b7b6f8489ca770940fbcbfd950e1cffbe1b5
Emre Ergun <emreergun@engramsoft.com>
2025-03-22 18:07:15 +0300

    vite plugin added to user interface side for top level await issue

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [wasm(), topLevelAwait()],
   server: {
     port: 3000,
   },
